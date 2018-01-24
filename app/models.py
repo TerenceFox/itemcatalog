d8b97bd586dc166a58366137f9c14a7a3f9800c8
Terence Fox <terence.fo@gmail.com>
2018-01-24 18:29:00 -0500

    Add database

--- app/models.py
+++ app/models.py
@@ -14,13 +14,58 @@ class User(db.Model):
                       nullable=False)
     picture = db.Column(
                         db.String(250),
-                        index=true,
+                        index=True,
                         unique=True)
     password_hash = db.Column(
                               db.String(128),
-                              index=true,
+                              index=True,
                               unique=True,
                               nullable=False)
+    categories = db.relationship('Category', backref='creator', lazy='dynamic')
+    items = db.relationship('Item', backref='creator', lazy='dynamic')
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(
+                         db.String(64),
+                         index=True,
+                         unique=True,
+                         nullable=False)
+    description = db.Column(
+                      db.String(250),
+                      index=True,
+                      unique=True,
+                      nullable=False)
+    items = db.relationship('Item', backref='items', lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def __repr__(self):
+        return '<Category {}>'.format(self.name)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(
+                         db.String(64),
+                         index=True,
+                         unique=True,
+                         nullable=False)
+    description = db.Column(
+                      db.String(250),
+                      index=True,
+                      unique=True,
+                      nullable=False)
+    picture = db.Column(
+                        db.String(250),
+                        index=True,
+                        unique=True)
+    category = db.Column(db.Integer, db.ForeignKey('category.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Item {}>'.format(self.name)
